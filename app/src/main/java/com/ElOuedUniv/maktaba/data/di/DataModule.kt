e416f7d0523c0d9f0e9bb277cb50f4b3f47a7c1d
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2026-04-26 23:16:15 +0100

    Switch to Supabase implementations for repositories

--- app/src/main/java/com/ElOuedUniv/maktaba/data/di/DataModule.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/di/DataModule.kt
@@ -1,9 +1,9 @@
 package com.ElOuedUniv.maktaba.data.di
 
 import com.ElOuedUniv.maktaba.data.repository.BookRepository
-import com.ElOuedUniv.maktaba.data.repository.BookRepositoryImpl
+import com.ElOuedUniv.maktaba.data.repository.SupabaseBookRepositoryImpl 
 import com.ElOuedUniv.maktaba.data.repository.CategoryRepository
-import com.ElOuedUniv.maktaba.data.repository.CategoryRepositoryImpl
+import com.ElOuedUniv.maktaba.data.repository.SupabaseCategoryRepositoryImpl 
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,16 +17,16 @@ object DataModule {
     @Provides
     @Singleton
     fun provideCategoryRepository(
-        categoryRepositoryImpl: CategoryRepositoryImpl
+        supabaseCategoryImpl: SupabaseCategoryRepositoryImpl 
     ): CategoryRepository {
-        return categoryRepositoryImpl
+        return supabaseCategoryImpl
     }
 
     @Provides
     @Singleton
     fun provideBookRepository(
-        bookRepositoryImpl: BookRepositoryImpl
+        supabaseBookImpl: SupabaseBookRepositoryImpl
     ): BookRepository {
-        return bookRepositoryImpl
+        return supabaseBookImpl
     }
 }
