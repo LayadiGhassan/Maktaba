83cb685dbe48909a40766c488d66d112eac05d05
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2026-04-26 18:46:54 +0100

    Fix method name and update TODO comment

--- app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/AddBookUseCase.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/domain/usecase/AddBookUseCase.kt
@@ -8,7 +8,7 @@ class AddBookUseCase @Inject constructor(
     private val bookRepository: BookRepository
 ) {
     suspend operator fun invoke(book: Book) {
-        // TODO: Exercise 3 - Call the repository to add the book
-        bookRepository.addbook(book)
+        // TODO: Exercise 3 - Call the repository to add the book ✅
+        bookRepository.addBook(book)
     }
 }
