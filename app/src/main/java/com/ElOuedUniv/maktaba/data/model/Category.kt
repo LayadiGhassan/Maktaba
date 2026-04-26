b16b37b151482f8f82e6f5c530857f02a747a036
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2026-04-26 22:29:35 +0100

    Annotate Category class with @Serializable
    Added @Serializable annotation to Category data class.

--- app/src/main/java/com/ElOuedUniv/maktaba/data/model/Category.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/model/Category.kt
@@ -1,6 +1,7 @@
 package com.ElOuedUniv.maktaba.data.model
 
 // TODO: Complete the Category data class implementation
+@Serializable
 data class Category(
     val id: String,
     val name: String,
