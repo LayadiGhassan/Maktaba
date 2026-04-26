e2503e45c7524f291f7091885fb05ca9870f0e59
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2026-04-26 23:28:11 +0100

    Integrate UserPreferences and modify NavGraph logic
    Inject UserPreferences and update NavGraph based on onboarding status.

--- app/src/main/java/com/ElOuedUniv/maktaba/MainActivity.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/MainActivity.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.ElOuedUniv.maktaba.data.local.UserPreferences
 import com.ElOuedUniv.maktaba.presentation.navigation.NavGraph
 import com.ElOuedUniv.maktaba.presentation.theme.MaktabaTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     
+    @Inject
+    lateinit var userPreferences: UserPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         
         setContent {
+            
+            val isOnboardingCompleted by userPreferences.isOnboardingCompleted
+                .collectAsState(initial = null)
+
             MaktabaTheme {
-                NavGraph()
+                
+                if (isOnboardingCompleted != null) {
+                    NavGraph(isOnboardingCompleted = isOnboardingCompleted ?: false)
+                }
             }
         }
     }
