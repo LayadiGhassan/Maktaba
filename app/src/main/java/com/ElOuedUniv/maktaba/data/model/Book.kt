817a611d74b40e578b24c4ab3610255ab995781c
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2026-04-26 22:52:41 +0100

    Refactor Book data class by removing SerialName annotations

--- app/src/main/java/com/ElOuedUniv/maktaba/data/model/Book.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/model/Book.kt
@@ -1,22 +1,11 @@
 package com.ElOuedUniv.maktaba.data.model
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerialName
 
 @Serializable
 data class Book(
-    @SerialName("isbn")
-    val isbn: String, 
-    
-    @SerialName("title")
+    val isbn: String,
     val title: String,
-    
-    @SerialName("nb_pages")
     val nbPages: Int,
-    
-    @SerialName("image_url")
-    val imageUrl: String? = null,
-    
-    @SerialName("category_id") 
-    val categoryId: Long? = null
+    val imageUrl: String? = null
 )
