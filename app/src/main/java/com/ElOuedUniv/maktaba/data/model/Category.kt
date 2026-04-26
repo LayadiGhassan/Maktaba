86b2ccd3bd4b7a1ba6e8c56b333b4a5fb4e47ec6
LayadiGhassan <115711413+LayadiGhassan@users.noreply.github.com>
2026-04-26 22:53:18 +0100

    Annotate Category class with Serializable
    Added kotlinx.serialization.Serializable annotation to Category data class.

--- app/src/main/java/com/ElOuedUniv/maktaba/data/model/Category.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/model/Category.kt
@@ -1,5 +1,7 @@
 package com.ElOuedUniv.maktaba.data.model
 
+import kotlinx.serialization.Serializable
+
 // TODO: Complete the Category data class implementation
 @Serializable
 data class Category(
